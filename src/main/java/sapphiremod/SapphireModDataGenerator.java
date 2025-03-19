f1ebb9a1cf6b515c0df5c1417487ab0f20ff9b78
samuel <samuel.kuhn@gmx.ch>
2025-03-19 21:35:27 +0100

    initial commit

---
+++ src/main/java/sapphiremod/SapphireModDataGenerator.java
@@ -0,0 +1,11 @@
+package sapphiremod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SapphireModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
