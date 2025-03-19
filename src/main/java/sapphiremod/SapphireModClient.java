d52720af232d587f12f6d32218217c782220620b
samuel <samuel.kuhn@gmx.ch>
2025-03-19 21:51:42 +0100

    first steps

---
+++ src/main/java/sapphiremod/SapphireModClient.java
@@ -0,0 +1,10 @@
+package sapphiremod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SapphireModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
