75aa22c07cdf9f2c934338aaba20a9886e4f4dc6
samuel <samuel.kuhn@gmx.ch>
2025-03-19 22:47:25 +0100

    added data generation

--- src/main/java/sapphiremod/SapphireModDataGenerator.java
+++ src/main/java/sapphiremod/SapphireModDataGenerator.java
@@ -2,10 +2,17 @@ package sapphiremod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import sapphiremod.datagen.*;
 
 public class SapphireModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelsProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
+		pack.addProvider(ModWorldGenerator::new);
+		pack.addProvider(ModBlockTagProvider::new);
 	}
 }
